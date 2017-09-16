e0222a56748225db058f1e6b3cfa046851ac6332
Lukas Elmer <lukas.elmer@gmail.com>
2017-09-16 23:04:44 +0200

    Refactor & small fixes

--- lib/business_logic.dart
+++ lib/business_logic.dart
@@ -3,29 +3,22 @@ import 'dart:async';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:hackzurich2017/firebase_helper.dart';
 
-Future<Null> afterLogin(String emailHash) async {
-
+Future<Null> afterLogin(String emailHash, String imageUrl) async {
   final DataSnapshot user = await db().child("users/emailHash").once();
+  if (user != null) return null;
 
-  if (user != null) {
-    return null;
-  }
-
-  final key = createGroup(user.value);
-  createUser(user.value["imageUrl"], emailHash, key);
+  final groupId = await _createGroup(emailHash, imageUrl);
+  await _createUser(user.value["imageUrl"], emailHash, groupId);
 }
 
-createUser(imageUrl, emailHash, key) {
-  db().child("users/${emailHash}").set({
-    "group": emailHash,
-    "imageUrl": imageUrl
-  });
+Future<String> _createGroup(emailHash, imageUrl) async {
+  final groupReference = db().child("groups").push();
+  await groupReference.set({emailHash: imageUrl});
+  return groupReference.key;
 }
 
-createGroup(user) {
-  final reference = db().child("groups").push();
-  reference.set({
-    user.emailHash: user.imageUrl
-  });
-  return reference.key;
+Future<Null> _createUser(String imageUrl, String emailHash, String groupId) {
+  return db()
+      .child("users/${emailHash}")
+      .set({"group": emailHash, "imageUrl": imageUrl});
 }
